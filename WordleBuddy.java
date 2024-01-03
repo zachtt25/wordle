f052c9b720201c6e2a8af9faa833bd58158089f8
AHS CS <ahs-cs@ausdk12.org>
2024-01-03 12:36:31 -0800

    commit

--- WordleBuddy.java
+++ WordleBuddy.java
@@ -8,6 +8,25 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class WordleBuddy {
+    ArrayList<String> possibleAnswers = new ArrayList<>();
+    public void setUp() {
+        try {
+            FileReader fileReader = new FileReader("answers.txt");
+         
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+         
+            while ((line = bufferedReader.readLine()) != null) {
+                possibleAnswers.add(line);
+            }
+            bufferedReader.close();
+            fileReader.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the file.");
+            e.printStackTrace();
+        }
+    }
+
     public int getGuessesUsed() {
         int guessesUsed = 0;
         Scanner scan = new Scanner(System.in);
@@ -35,12 +54,6 @@ public class WordleBuddy {
         return guessesUsed;
     }
 
-    public String getAnswer() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("What was today's wordle answer?");
-        return scan.nextLine();
-    }
-
     ArrayList<String> guesses = new ArrayList<>();
     public void getGuesses(int guessesUsed) {
         for (int i = 1; i <= guessesUsed; i++) {
@@ -48,31 +61,41 @@ public class WordleBuddy {
             System.out.println("What was your guess #" + i);
             guesses.add(scan.nextLine());
         }
-
     }
-    public static void main(String[] args) {
-        ArrayList<String> possibleAnswers = new ArrayList<>();
-        try {
-            FileReader fileReader = new FileReader("answers.txt");
-         
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String line;
-         
-            while ((line = bufferedReader.readLine()) != null) {
-                possibleAnswers.add(line);
+    public void getColorOfGuess() {
+        ArrayList<String> colors = new ArrayList<>();
+        for (int i = 0; i <= guesses.size(); i++) {
+            for (int j = 0; i <= 4; i++) {
+                Scanner scan = new Scanner(System.in);
+                System.out.println("'" + guesses.get(i) + "'" + " was your first guess; What color was the letter" + "'" + (guesses.get(i)).substring(j, j+1) +"'? ('green', 'yellow', 'grey')");
+                colors.add(scan.nextLine());
             }
-            bufferedReader.close();
-            fileReader.close();
-        } catch (IOException e) {
-            System.out.println("An error occurred while reading the file.");
-            e.printStackTrace();
         }
-        System.out.println(possibleAnswers.get(0));
+    }
 
+    public String returnGuessScore(int guessNumber) {
+        int x = possibleAnswers.size();
+        ArrayList<String> newPossibleAnswers = new ArrayList<>();
+        for (int i = 0; i <= x; i++) {
+            
+        }
+        int y = possibleAnswers.size() - newPossibleAnswers.size();
+        double z = y / possibleAnswers.size();
+        ArrayList<String> possibleAnswers = newPossibleAnswers;
+        return "This guess got a score of " + z + "! You eleminated " + y + " out of " + x + " possible answers";
+    }
 
+    public void runBuddy() {
+        for (int i = 1; i <= getGuessesUsed(); i++) {
+            returnGuessScore(i);
+        }
+    }
+
+    public static void main(String[] args) {
         WordleBuddy wb = new WordleBuddy();
+        wb.setUp();
         int guessesUsed = wb.getGuessesUsed();
         wb.getGuesses(guessesUsed);
-        System.out.println(wb.guesses);
+        wb.runBuddy();
    }
 }
